57834705abd5eb0d8cd9f91c5a355acce56d67a8
walimike <walimike139@gmail.com>
2018-11-09 21:02:01 +0300

    add js to select users [#161746587]

---
+++ UI/scripts/main.js
@@ -0,0 +1,6 @@
+function SelectUser(){
+	if (document.getElementById('admin').checked){
+		window.location.href ="adminhome.html";}
+  else if (document.getElementById('user').checked){
+								window.location.href ="userhome.html";}
+}
