e5c8b0b5719ad6b5bf916e2ba1facd7de9f2cee9
walimike <walimike139@gmail.com>
2018-11-10 17:45:54 +0300

    add views for welcome message [delivers #161859725]

--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
 from flask import Blueprint, jsonify, abort, request, make_response, json
 
 appblueprint = Blueprint('api',__name__)
+
+@appblueprint.route('/')
+def welcomeMessage():
+    return "<h1>Welcome to Send It.</h1>"
