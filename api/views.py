eaae73b3d76285268a87e3db52919df6f786ac89
walimike <walimike139@gmail.com>
2018-11-11 14:05:57 +0300

    add endpoint for add_order

--- api/views.py
+++ api/views.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, jsonify, abort, request, make_response, json
 from api.models.parcels import ParcelList
+from api.models.models import Parcel
+from api.models.validators import Validator
 
-my_parcels = ParcelList()
+
 appblueprint = Blueprint('api',__name__)
+my_parcels = ParcelList()
+is_valid = Validator()
+
 
 @appblueprint.route('/')
 def welcomeMessage():
@@ -11,3 +16,26 @@ def welcomeMessage():
 @appblueprint.route('/parcels')
 def fetch_all_orders():
     return jsonify({"Parcel list": my_parcels.parcel_list})
+
+@appblueprint.route('/parcels', methods=['POST'])
+def make_order():
+    """{"Owner":"","Source":"","Destination":"","Parcel name":""}"""
+    if not request.json or not request.get_json()['Owner'] or not \
+    request.get_json()['Source'] or not request.get_json()['Destination']\
+    or not request.get_json()['Parcel name']:
+        abort (400)
+
+    owner,source,destination,parcel_name = request.get_json()['Owner'],\
+    request.get_json()['Source'], request.get_json()['Destination'],\
+    request.get_json()['Parcel name']
+
+    if not is_valid.input_fields(owner,parcel_name,source,destination):
+        return jsonify({"Error":"Ooops, one of the input fields is not in order"}), 400
+
+    new_parcel = Parcel(parcel_name,source,destination)
+    my_parcels.add_parcel(owner,new_parcel)
+    return jsonify({"ParcelList":my_parcels.parcel_list}), 201
+
+@appblueprint.route('/users')
+def fetch_all_users():
+    return jsonify({"Userlist":my_parcels.fetch_all_users()})
