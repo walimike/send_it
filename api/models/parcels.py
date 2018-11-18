ef8bb193ceb927b1d683a975842ebf92c28665d6
walimike <walimike139@gmail.com>
2018-11-18 18:15:10 +0300

    fixes db connection child classes [delivers #162035411]

--- api/models/parcels.py
+++ api/models/parcels.py
@@ -1,12 +1,29 @@
 from api.models.models import Parcel, User
-from api.models.db_controller import Dbcontroller
-
+from api.models.dbconnection import Dbcontroller
 class ParcelDb(Dbcontroller):
 
-    def __init__(self,database_url):
-        super().__init__(database_url)
+    def __init__(self):
+        super().__init__()
 
-    def add_parcel(self,new_parcel):
+    def add_parcel(self,parcel):
         self.cursor.execute("INSERT INTO parcels(parcel_name,price,\
-        parcel_status,usrId) VALUES(%s, %s, %s, %s);",\
-        (new_parcel.name,new_parcel.price,new_parcel.status,new_parcel.id))
+        parcel_status,usrId,parcel_source,parcel_destination,present_location)\
+        VALUES(%s, %s, %s, %s, %s, %s, %s);",(parcel.name,parcel.price,\
+        parcel.status,parcel.id,parcel.source,parcel.destination,parcel.location))
+
+    def fetch_all_orders(self):
+        return self.fetch_all_entries('parcels')
+
+    def fetch_parcel(self,id):
+        """Returns a user in form of a dict or None if user not found"""
+        query = "SELECT * FROM parcels WHERE parcelid=%s"
+        self.cursor.execute(query, (id,))
+        parcel = self.cursor.fetchone()
+        return parcel
+
+    def fetch_parcel_by_specific(self,id):
+        """Returns a user in form of a dict or None if user not found"""
+        query = "SELECT * FROM parcels WHERE usrid=%s"
+        self.cursor.execute(query, (id,))
+        parcels = self.cursor.fetchall()
+        return parcels
