20b66d49a034184fe5e00e00c4a59fe692fdd70b
walimike <walimike139@gmail.com>
2018-11-18 22:42:55 +0300

    add endpoint for user signup [delivers #162038615]

--- api/views/auth.py
+++ api/views/auth.py
@@ -11,3 +11,20 @@ appblueprint = Blueprint('api',__name__)
 parcel_db = ParcelDb()
 user_db = UserDb()
 user_db.create_tables()
+
+
+@appblueprint.route('/auth/signup', methods=['POST'])
+def add_user():
+    """{"Name":"","Email":"","Password":"","Role":""}"""
+    if not request.json or not request.get_json()['Name'] or not \
+    request.get_json()['Email'] or not request.get_json()['Password']\
+    or not request.get_json()['Role']:
+        return jsonify({"msg": "Bad request"}), 400
+
+    name,email,password,role = request.get_json()['Name'],\
+    request.get_json()['Email'], request.get_json()['Password'],\
+    request.get_json()['Role']
+
+    new_user = User(name,email,password,role)
+    user_db.add_user(new_user)
+    return jsonify({"User credentials":user_db.fetch_user(new_user)}),200
