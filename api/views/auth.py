12f4241747f83f5daea2192680b37c3fdcc0679f
walimike <walimike139@gmail.com>
2018-11-20 05:20:58 +0300

    update import statement and fix signup code [#162076058]

--- api/views/auth.py
+++ api/views/auth.py
@@ -1,34 +1,26 @@
+from api.views.utilities import parcel_db, user_db, is_not_valid_login_json,is_not_valid_signup_json
 from flask import Blueprint, jsonify, abort, request, json
-from api.models.parcels import ParcelDb
-from api.models.models import User
-from api.models.users import UserDb
-from flask_jwt_extended import (
-    JWTManager, jwt_required, create_access_token,
+from flask_jwt_extended import (JWTManager, jwt_required, create_access_token,
     get_jwt_identity)
-
-
-appblueprint = Blueprint('api',__name__)
-parcel_db = ParcelDb()
-user_db = UserDb()
-user_db.create_tables()
+from api.views.utilities import appblueprint
+from api.models.models import User
 
 
 @appblueprint.route('/auth/signup', methods=['POST'])
 def add_user():
     """{"Name":"","Email":"","Password":"","Role":""}"""
-    if not request.json or not request.json.get('Name') or not \
-    request.json.get('Email') or not request.json.get('Password')\
-    or not request.json.get('Role'):
-        abort(400)
+    user_input = request.json
+    if is_not_valid_signup_json(user_input):
+        return is_not_valid_signup_json(user_input)
 
-    name,email,password,role = request.json.get('Name', None),\
-    request.json.get('Email', None), request.json.get('Password', None),\
-    request.json.get('Role', None)
+    name = request.json.get('Name', None)
+    email = request.json.get('Email', None)
+    password = request.json.get('Password', None)
+    role = request.json.get('Role', None)
 
     new_user = User(name,email,password,role)
     user_db.add_user(new_user)
     return jsonify({"User credentials":user_db.fetch_user(new_user)}),201
-
 
 @appblueprint.route('/auth/login', methods=['POST'])
 def login():
@@ -44,6 +36,6 @@ def login():
 
     new_user = User(username,' ',password, ' ')
     current_user = user_db.fetch_user(new_user)
-    
+
     access_token = create_access_token(identity=current_user["usrid"])
     return jsonify({"Message":"Successfully loged in"}), 200
