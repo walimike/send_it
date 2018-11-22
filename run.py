5de4adc6a4bc2f06174b3d3d06aa570eca486e31
walimike <walimike139@gmail.com>
2018-11-22 10:00:31 +0300

    fix drop db bug

--- run.py
+++ run.py
@@ -3,7 +3,8 @@ from api.views.utilities import user_db
 from api.models.models import User
 
 if __name__ == '__main__':
-    admin_user = User('admin','admin@gmail.com','@H@nN@H92','admin')
+    admin_user = User('admin_user','admin@gmail.com','@H@nN@H92','admin')
+    user_db.drop_tables()
     user_db.create_tables()
     user_db.add_user(admin_user)
     app.run()
