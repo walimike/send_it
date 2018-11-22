2c244ad1132a0cada0649c411280f52efc2e17ec
walimike <walimike139@gmail.com>
2018-11-22 09:42:14 +0300

    fix build bug

--- run.py
+++ run.py
@@ -4,9 +4,6 @@ from api.models.models import User
 
 if __name__ == '__main__':
     admin_user = User('admin','admin@gmail.com','@H@nN@H92','admin')
-    normal_user = User('user','user@ymail.com','S33d0f@by','user')
-    user_db.drop_tables()
     user_db.create_tables()
     user_db.add_user(admin_user)
-    user_db.add_user(normal_user)
     app.run()
