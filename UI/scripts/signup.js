361f355dd005c6ce9e26ac5f47093d324dd075f2
walimike <walimike139@gmail.com>
2018-11-30 14:39:47 +0300

    fix signup endpoint

--- UI/scripts/signup.js
+++ UI/scripts/signup.js
@@ -1,25 +1,20 @@
-document.getElementById('signupForm').addEventListener('submit', registerUser);
-const url = 'https://walimike-sendit2.herokuapp.com/v2/api/auth/signup';
-
-function registerUser(event){
-    event.preventDefault();
-
+function signUp() {
     let username = document.getElementById('username').value;
-    let email = document.getElementById('email').value;
     let password1 = document.getElementById('password1').value;
     let password2 = document.getElementById('password2').value;
-    let role = document.getElementsById('user-type').value;
-
+    let email = document.getElementById('email').value;
+    
     if (password1 != password2){
-        alert("passwords do not match")
-    }
-    let data = {
-        username: username,
-        email: email,
-        password: password1,
-        role: role
+        alert('Passwords do not match')
     }
 
+    const url = 'https://walimike-sendit2.herokuapp.com/v2/api/auth/signup';
+
+    let data = {
+        name: username,
+        email: email,
+        password: password1
+    }
     fetch(url, {
         method: 'POST',
         mode: 'cors',
@@ -28,17 +23,8 @@ function registerUser(event){
     })
     .then(res => res.json())
     .then(response => {
-        console.log(response);
-        if (response.message === 'you have successfully signed up') {
-            alert(`You have registered as ${data['username']}`);
-            window.location.replace('index.html');
-        } else {
-            alert(response.message);
-        }
+        alert(response.message);
+        window.location.replace('index.html');
     })
-    .catch(err => console.log(err));
 }
-d
-function signUp() {
-    alert("Successfully signed up, go to log in"+role);
-}
+    
